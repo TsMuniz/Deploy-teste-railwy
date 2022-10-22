ef93477193cbea85723a0ec2f4541153dfd4ee8e
thiago <ts_muniz@outlook.com>
2022-10-22 09:58:13 -0300

    feat: adiciona rota de testes

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => res.status(201).json({ola: "mundo"}))
+app.get('/teste', (req, res) => res.sendStatus(404))
 
 app.listen(PORT, () => "rodando na 5000")
