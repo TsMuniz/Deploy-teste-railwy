7b382488b6add96898107236ea4116a752e58a9c
thiago <ts_muniz@outlook.com>
2022-10-22 10:00:30 -0300

    feat: adiciona mensagem de teste

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => res.status(201).json({ola: "mundo"}))
-app.get('/teste', (req, res) => res.sendStatus(404))
+app.get('/teste', (req, res) => res.status(200).json({teste: "testando alguma coisa agora"}))
 
 app.listen(PORT, () => "rodando na 5000")
